8166697a4a5748ddb6a6336e2ed493b9864ce4d3
Abuzar Mughal <abuzar2407@hotmail.com>
2019-01-03 17:09:40 +0500

    with node_modules
    with node_modules

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
 
   private loggedInStatus = false;
   private token = '';
-  private url = 'http://46.101.57.72:3000';
+  private url = environment.baseUrl;
   private options;
   constructor(private http: HttpClient, private router: Router) {
         this.loggedInCheck();
